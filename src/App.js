5fab31f180eb1485ee15d20f23cdeff9983a84c7
anilkumawat7788 <71585735+anilkumawat7788@users.noreply.github.com>
2021-03-04 23:59:05 +0530

    firstchange

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PageScroll from './components/pagescroll/PageScroll';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Blog from './components/blog/Blog';
+import Career from './components/careers/Career';
+import Product from './components/product/Product';
+import Service from './components/services/Service';
 
+import Navbar from './components/header/Navbar';
+import Footer from './components/footer/Footer'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Router>
+   <PageScroll />
+   <Navbar />
+     <Switch>
+       <Route exact path='/' component={Home} />
+       <Route exact path='/about' component={About} />
+       <Route exact path='/service' component={Service} />
+       <Route exact path='/product' component={Product} />
+       <Route exact path='/blog' component={Blog} />
+       <Route exact path='/career' component={Career} />
+       <Route exact path='/contact' component={Contact} />
+       
+     </Switch>
+     <Footer />
+   </Router>
+
+   </>
   );
 }
 
